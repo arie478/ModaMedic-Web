69fd4bb0124744fb55f06602223a5a09ca5dcfc3
noyharar <noyharar@post.bgu.ac.il>
2020-12-29 18:15:14 +0200

    messages page of patient

--- src/MessagesPage.js
+++ src/MessagesPage.js
@@ -3,62 +3,106 @@ import {Form} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import 'bootstrap/dist/css/bootstrap.css'
+import axios from "axios";
 
 class MessagesPage extends Component {
 
     constructor() {
         super();
-        var date = new Date();
         this.state = {
-            sendFrom: '',
-            sendTo: '',
-            messageInfo:'',
-            timeSumbit: date.toISOString().split("T")[0]
+            messages:[],
+            content:'כתוב את הודעתך כאן'
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    componentDidMount() {
+        if (sessionStorage.getItem('patient')) {
+            this.fetchMessages();
+        }
+    }
+
+    async fetchMessages(){
+        var response = await axios.get(
+            "http://localhost:8180/auth/patients/messages",
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-auth-token': sessionStorage.getItem("token")
+                }
+            }
+        );
+        if(response.data.data) {
+            this.setState({messages: response.data.data});
+        }
+    }
+
+    async addMessage(){
+        axios.post('http://localhost:8180/auth/patients/messages',
+            {
+                content: this.state.content,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-auth-token': sessionStorage.getItem("token")
+                }
+            }).then(res => {
+            window.alert("ההודעה נוספה בהצלחה!");
+            this.fetchMessages();
+        });
+    }
+
+
+
+    handleChange(event) {
+        this.setState({content: event.target.value});
+}
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.addMessage();
     }
 
     render() {
         require("./MessagesPage.css");
+
         return (
             <div>
-                <Form>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>כתוב הודעה בתיבת ההודעות:</Form.Label>
-                        <Form.Control as="textarea" rows={4} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-<br/>
-<div class="tableMessages">
-                <Table id="mdd" striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>תאריך</th>
-                        <th>מוען</th>
-                        <th>תוכן ההודעה</th>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <textarea value={this.state.content} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <br/>
+                {this.state.messages.length > 0 &&
+                <div class="tableMessages">
+                    <Table id="mdd" striped bordered hover>
+                        <thead>
+                        <tr>
+                            {/*<th>#</th>*/}
+                            <th>תאריך</th>
+                            <th>מוען</th>
+                            <th>תוכן ההודעה</th>
 
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>eltrejkto</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    </tbody>
-                </Table>
-            </div>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.messages.map((message) => (
+                                <tr>
+                                    <td>{new Date(message.Date).toLocaleString()}</td>
+                                    <td>{`${message.FromFirstName} ${message.FromLastName}`}</td>
+                                    <td>{message.Content}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>}
+                {this.state.messages.length === 0 &&
+                <p> אין הודעות </p>
+                }
             </div>
         )
     }
