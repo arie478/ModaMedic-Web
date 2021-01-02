715ec3ad62c5c960d4c16d33efe68d88c312c90e
noyharar <noyharar@post.bgu.ac.il>
2021-01-02 19:03:57 +0200

    try to put last version

--- src/Questionnaire/Questionnaire.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import SurveyComponent from './SurveyComponent';
-
-
-ReactDOM.render(<SurveyComponent />, document.getElementById('surveyElement'));
